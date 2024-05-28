98dee34edd790f61987c277caaca8381df1f7a11
grace0950 <pinyan0950@gmail.com>
2024-05-29 01:12:22 +0800

    chore: Enable image upload and implement authentication guards

--- src/controllers/upload-image.controller.ts
+++ src/controllers/upload-image.controller.ts
@@ -7,10 +7,13 @@ import {
   Post,
   Res,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiConsumes } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/guard/jwt-auth.guard';
+import { UserGuard } from 'src/guard/user.guard';
 import { UploadImageService } from 'src/upload-image/upload-image.service';
 
 @Controller('upload-image')
@@ -18,6 +21,7 @@ export class UploadImageController {
   constructor(private readonly uploadImageService: UploadImageService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, UserGuard)
   @UseInterceptors(FileInterceptor('file'))
   async uploadImage(@UploadedFile() file: Express.Multer.File, @Res() res) {
     const fileName = await this.uploadImageService.uploadImage(file);
@@ -25,12 +29,14 @@ export class UploadImageController {
   }
 
   @Get('/:fileName')
+  @UseGuards(JwtAuthGuard, UserGuard)
   async getImageUrl(@Param('fileName') fileName: string, @Res() res) {
     const url = await this.uploadImageService.getImageUrl(fileName);
     return res.status(HttpStatus.OK).json({ url });
   }
 
   @Delete('/:fileName')
+  @UseGuards(JwtAuthGuard, UserGuard)
   async deleteImage(@Param('fileName') fileName: string, @Res() res) {
     await this.uploadImageService.deleteImage(fileName);
     return res.status(HttpStatus.NO_CONTENT).send();
